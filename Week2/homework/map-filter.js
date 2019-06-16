95935ac815895fa67bd0af2801ab2c35deb8e02f
Catsudemo <cattmott@gmail.com>
2019-06-16 11:38:16 +0200

    Updated mapfilter homework

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  return numbers.map(number => (number % 2 !== 0 ? number * 2 : number));
 }
 
 const myNumbers = [1, 2, 3, 4];
